bce68e1432b403de47b69fd771499a50e65c1da0
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-10-08 14:59:03 +0400

    + nop/bye run
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- FORTH.c
+++ FORTH.c
@@ -16,14 +16,36 @@ void Bcompile(BYTE byte) {
 	M[Cp++] = byte;
 }
 
+void NOP() {
+	#ifdef VM_TRACELOG
+		printf("nop");
+	#endif
+}
+
+void BYE() {
+	#ifdef VM_TRACELOG
+		printf("bye\n\n");
+	#endif
+	exit(0);
+}
+
 void VM() {
 	Ip=0;						/* reset Ip */
 	uint8_t op;					/* opcode register */
 	for (;;) {					/* infty bytecode interpreter loop */
 		op = M[Ip++];
+		#ifdef VM_TRACELOG
+			printf("\n%.8X:\t%.2X\t",Ip-1,op);
+		#endif
 		switch (op) {			/* we'll move to command function table later */
+			case op_NOP: NOP(); break;
+			case op_BYE: BYE(); break;
 			default:
-				fprintf(stderr,"%.8X:\t%.2X\n\nundefined opcode\n\n",Ip-1,op);
+				#ifdef VM_TRACELOG
+					printf("undefined opcode\n\n");
+				#else
+					fprintf(stderr,"\n%.8X:\t%.2X\tundefined opcode\n\n",Ip-1,op);
+				#endif
 				abort();
 		}
 	}
--- doc/HAL.md
+++ doc/HAL.md
@@ -1,5 +1,7 @@
 # Hardware Abstraction Layer {#hal}
 
+@module hal Hardware Abstraction Layer
+
 HAL provides unified API to different hardware via 
 * specific low-level drivers for every computer module type, or 
 * host OS API wrappers for emulated mode.
@@ -9,3 +11,7 @@ can be run portable between many platforms.
 
 **HAL must stay a thin layer**, else you will get not a little fun hardware console, but yet another Linux.
 
+## Supported (hardware) platforms
+
+* @ref nuc
+* @ref halk
