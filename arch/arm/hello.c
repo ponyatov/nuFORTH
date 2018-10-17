d779ce21bbedd625ef54b2b03fa39c796ad6d7b1
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-10-17 18:03:58 +0400

    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- arch/arm/hello.c
+++ arch/arm/hello.c
@@ -4,7 +4,7 @@
 
 // https://balau82.wordpress.com/2010/02/28/hello-world-for-bare-metal-arm-using-qemu/
 
-volatile const uint32_t *UART0DR = (uint32_t *)0x101f1000;	// UART0 data register
+volatile uint32_t *UART0DR = (uint32_t *)0x101f1000;	// UART0 data register
 
 // QEMU model of the PL011 serial port ignores the transmit FIFO capabilities
 // on real systems the `Transmit FIFO Full` flag must be checked
