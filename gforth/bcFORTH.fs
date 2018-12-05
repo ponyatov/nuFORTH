88c8f78430adfd01dd49a14f5c5c5155b47ff9d4
Dmitry Ponyatov <dponiatov@icbcom.ru>
2018-12-05 18:19:35 +0400

    + sizes, SAVE
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- gforth/bcFORTH.fs
+++ gforth/bcFORTH.fs
@@ -1,17 +1,28 @@
 \ https://groups.google.com/forum/#!topic/comp.lang.forth/eYiPcW8uPRY
 
-VOCABULARY TC	\ target compiler vocabulary
+\ portable bytecode target compiler (pure FORTH in gFORTH)
+\ (c) Dmitry Ponyatov <dponyatov@gmail.com>, public domain
+\ github: https://github.com/ponyatov/nuFORTH/tree/dev/gforth
 
-\ words			\ will print main system vocabulary
+VOCABULARY TC					\ create target compiler vocabulary
+
+\ words							\ will print main system vocabulary
 
 ONLY FORTH ALSO TC DEFINITIONS	\ configure vocabulary search order/compiling
 
+0x1000 constant Msz				\ main memory size (bytes)
+0x100  constant Rsz				\ return stack size (cells)
+0x10   constant Dsz				\ data stack size (cells)
+
+words							\ will print only target compiler vocabulary
+
+cr Msz . Rsz . Dsz . cr cr
+
 words
 
-0x1000 constant Msz	\ main memory size
+: SAVE ( filename.bc -- ) \ write bytecode to image file
+	( filename ) w/o create-file 
+;
 
-Msz .
-words
-
-bye
+S" FORTH.bc" SAVE BYE			\ write compiled system and exit (batch build)
 
