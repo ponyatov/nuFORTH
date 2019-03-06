2b0a58e9be5dfb5ab95f7f954b76a22bdb5c8105
Dmitry Ponyatov <dponiatov@icbcom.ru>
2019-03-06 12:23:43 +0400

    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

--- STM32_meta/STM32FORTH.c
+++ STM32_meta/STM32FORTH.c
@@ -3,17 +3,35 @@
 ///
 /// https://groups.google.com/d/msg/comp.lang.forth/eYiPcW8uPRY/Er4vVzJVDgAJ
 
+/// data stack size, cells
+#define Dsz 0x10
+
+#include <stdint.h>
+
+/// FreeRTOS target project tasks, queues and synchronizations will be here
+#include "FreeRTOS.h"
+#include "cmsis_os.h"
+#include "config.h"
+
 /// FVM machine word
-#define int32_t CELL
+typedef union { int n ; void* p; char* s; } CELL;
 /// FVM size of CELL in bytes
 #define CELLsz (sizeof(CELL))
 
-/// Data Stack size
+/// Data Stack
 CELL D[Dsz];
 /// Data Stack pointer
 uint8_t Dp=0;
 
+void inline PUSH(CELL c)	{ D[Dp++]=c;		}
+CELL inline POP(void)		{ return D[--Dp];	}
+
+void PRINT(void)			{ osMessagePut(qLOGHandle,(uint32_t)POP().s,0); }
+
 /// FORTH init point
 void FORTH(void) {
+	PUSH((CELL)"\033[33;7mSTM32\033[0m FORTH"); PRINT();
+	for (;;) {
 
+	}
 }
